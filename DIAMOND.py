71041664387b6e35cd0d603d32ad81d28e13e339
sandeep kumar chaurasia <58679551+sandeepkrc@users.noreply.github.com>
2021-01-29 23:58:10 +0530

    Add files via upload

---
+++ DIAMOND.py
@@ -0,0 +1,20 @@
+####  PRIMRE NO0
+n=int(input("n value:  "))
+for i in range(n):
+    print(" "*(n-i-1)+"*"*(i+1)+"*"*i)
+for i in range(n-1):
+    print(" "*(i+1)+"*"*(n-i-1)+"*"*(n-2-i))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
